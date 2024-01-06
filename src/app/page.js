edefc040594c756bb68c905d8af0d0fdc7f77712
kodiakcain <77345292+kodiakcain@users.noreply.github.com>
2024-01-06 02:42:03 -0500

    final push of night

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,6 @@ import { motion, useAnimation } from 'framer-motion';
 import Particle from './components/Particle'
 
 export default function Home() {
-
   const [isMounted, setIsMounted] = useState(false);
   const controls = useAnimation();
 
@@ -31,51 +30,45 @@ export default function Home() {
       animate={controls}
       transition={{ duration: 0.25 }}
     >
-      <header style={{top: '5vh'}}>
-        <head>
-          <title>Cains Portfolio</title>
-        </head>
-        <div style={{flexDirection: 'row', display: 'flex'}}>
+      <div style={{ flexDirection: 'row', display: 'flex' }}>
         <div className='particlets'>
-          <Particle></Particle>
+          <Particle />
         </div>
-        <div className='tsparticles'>
-        </div>
-          <div  className='topDiv' style={{display: 'flex', flexDirection: 'row', padding: '10px',}}>
+        <div className='tsparticles'></div>
+      </div>
+      <header style={{ top: '5vh' }}>
+        <div className='topDiv' style={{ display: 'flex', flexDirection: 'row', padding: '10px' }}>
           <p className={`text-4xl font-bold firstWordName`}>cain </p>
           <p className={`text-4xl font-bold secondWordName`}>clifton</p>
-          </div>
-          <div className='topDivLinks'>
-            <a href="https://github.com/kodiakcain" target='_blank' className='text-4xl font-bold linkPad'>
-            <GithubButton></GithubButton>
-            </a>
-            <a href="https://www.linkedin.com/in/cain-clifton/" target='_blank' className='text-4xl font-bold linkPad'>
-            <LinkedinButton></LinkedinButton>
-            </a>
-            <a href="mailto:americancain@gmail.com?subject=Message from Portfolio" target='_blank' className='text-4xl font-bold'><GmailButton></GmailButton></a>
-          </div>
         </div>
-        </header>
-    <main className="flex min-h-screen flex-col justify-between p-6">
+        <div className='topDivLinks'>
+          <a href="https://github.com/kodiakcain" target='_blank' className='text-4xl font-bold linkPad'>
+            <GithubButton></GithubButton>
+          </a>
+          <a href="https://www.linkedin.com/in/cain-clifton/" target='_blank' className='text-4xl font-bold linkPad'>
+            <LinkedinButton></LinkedinButton>
+          </a>
+          <a href="mailto:americancain@gmail.com?subject=Message from Portfolio" target='_blank' className='text-4xl font-bold'><GmailButton></GmailButton></a>
+        </div>
+      </header>
+      <main className="flex min-h-screen flex-col justify-between p-6">
+        {/* Rest of your content */}
         <div className='midDiv'>
-          <div style={{paddingRight: '10px'}}>
-          <Sidebar ></Sidebar>
-          
+          <div style={{ paddingRight: '10px' }}>
+            <Sidebar />
           </div>
           <div className='leftPad'>
-          <p className='text-4xl font-bold slogan'>Elevating concepts through</p>
+            <p className='text-4xl font-bold slogan'>Elevating concepts through</p>
             <p className='text-4xl font-bold sloganBelow'>the power of <span className='sloganFinal'>digital innovation.</span></p>
           </div>
         </div>
         <div className='rotatingLogo'>
-        <Link href='/Projects'>
-          <RotatingLogo></RotatingLogo>
-          </Link>   
+          <Link href='/Projects'>
+            <RotatingLogo />
+          </Link>
         </div>
-        
         <img src='/Cain.png' alt='Cain Image' className='avatar' />
-    </main>
-    
+      </main>
     </motion.div>
   );
 }
